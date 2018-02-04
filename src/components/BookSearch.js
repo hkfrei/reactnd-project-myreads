0ba154855145c5ccd8eec5113efdcf872a2f4a0c
Hanskaspar Frei <hkfrei@karten-werk.ch>
2018-02-04 14:10:27 +0100

    feat: Add a book component

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -20,7 +20,9 @@ class BookSearch extends React.Component {
       return
     }
     BooksAPI.search(query).then((response) => {
-      this.setState({ searchResults: response })
+      if (Array.isArray(response)) {
+        this.setState({ searchResults: response })
+      }
     })
   }
 
@@ -36,14 +38,6 @@ class BookSearch extends React.Component {
             Close
           </Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
             <input
               type="text"
               placeholder="Search by title or author"
